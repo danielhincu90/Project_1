4ace11a9657f42543b99336afb476cbd4635acb1
aacer <rthgfdtghg@ff>
2024-12-26 13:30:43 +0100

    database st

---
+++ src/config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_env_or_raise(key: str) -> str:
+    value = os.getenv(key)
+    if value is None:
+        raise ValueError(f"Environment variable {key} is not set")
+    return value
+
+DATABASE_URL = get_env_or_raise("DATABASE_URL")
