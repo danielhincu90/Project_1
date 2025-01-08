694247b3c3452700ccf1220cbbb2fbf59f915355
aacer <rthgfdtghg@ff>
2025-01-08 11:29:26 +0100

    models

--- src/models.py
+++ src/models.py
@@ -1,5 +1,7 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
 from src.database import Base
+
 
 class User(Base):
     __tablename__ = "users"
@@ -8,3 +10,25 @@ class User(Base):
     email = Column(String, unique=True, index=True)
     username = Column(String, unique=True, index=True)
     hashed_password = Column(String)
+
+    todos = relationship("Todo", back_populates="user", cascade="all, delete-orphan")
+
+class Todo(Base):
+    __tablename__ = "todos"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String, index=True, nullable=True)
+    completed = Column(Boolean, default=False)
+
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    user = relationship("User", back_populates="todos")
+
+class Category(Base):
+    __tablename__ = "categories"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
+    description = Column(String, index=True, nullable=True)
+
+    todos = relationship("Todo", back_populates="category")
