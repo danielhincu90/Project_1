942da876cdca419ed45cfc1019d515e4ce4306c7
aacer <rthgfdtghg@ff>
2025-01-08 11:29:09 +0100

    main

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
-from src.routes import router
+
+from src.routes.user_routes import router as router_users
+from src.routes.todo_routes import router as router_todo
+from src.routes.category_routes import router as router_category
 from src.database import Base, engine
 
-# Create database tables
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-app.include_router(router)
+app.include_router(router_users)
+app.include_router(router_todo)
+app.include_router(router_category)
